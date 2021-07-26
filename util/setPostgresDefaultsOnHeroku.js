deae88d6ae132973c22ac3ecd731a9ea96310f4a
Nicolas.G <gadnernicolas@gmail.com>
2021-07-26 14:48:46 +0200

    Added env files

--- util/setPostgresDefaultsOnHeroku.js
+++ util/setPostgresDefaultsOnHeroku.js
@@ -3,12 +3,16 @@ module.exports = function setPostgresDefaultsOnHeroku() {
     const { parse } = require('pg-connection-string');
 
     // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password } = parse(process.env.DATABASE_URL);
+    const { baseurl, host, database, user, password, salt } = parse(
+      process.env.DATABASE_URL,
+    );
 
     // Set standard environment variables
+    process.env.API_BASE_URL = baseurl;
     process.env.PGHOST = host;
     process.env.PGDATABASE = database;
     process.env.PGUSERNAME = user;
     process.env.PGPASSWORD = password;
+    process.env.CSRF_SECRET_SALT = salt;
   }
 };
