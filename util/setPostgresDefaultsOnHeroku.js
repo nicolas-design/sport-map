579040bda52dbf8e8d6b17c1d5361984d90e9a87
Nicolas.G <gadnernicolas@gmail.com>
2021-07-26 15:13:27 +0200

    Undid changes

--- util/setPostgresDefaultsOnHeroku.js
+++ util/setPostgresDefaultsOnHeroku.js
@@ -3,15 +3,12 @@ module.exports = function setPostgresDefaultsOnHeroku() {
     const { parse } = require('pg-connection-string');
 
     // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password, secret } = parse(
-      process.env.DATABASE_URL,
-    );
+    const { host, database, user, password } = parse(process.env.DATABASE_URL);
 
     // Set standard environment variables
     process.env.PGHOST = host;
     process.env.PGDATABASE = database;
     process.env.PGUSERNAME = user;
     process.env.PGPASSWORD = password;
-    process.env.CSRF_SECRET_SALT = secret;
   }
 };
